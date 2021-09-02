76aa0e87f48392283c487d66a9bb5dbfdb4f58dc
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-09-02 19:54:37 +0430

    work on unit test for delete tasks

--- src/test/task.test.js
+++ src/test/task.test.js
@@ -1,4 +1,4 @@
-import { addTask } from '../task.js';
+import { addTask, removeTask } from '../task.js';
 
 describe('add a task method', () => {
   // Arrange
@@ -8,7 +8,6 @@ describe('add a task method', () => {
 
   // Act
   addTask(tasks, description);
-  console.log(tasks[0] === output);
   // Assert
   test('should add a task', () => {
     expect(tasks.length).toBe(1);
@@ -17,4 +16,40 @@ describe('add a task method', () => {
   test('should add the correct object', () => {
     expect(tasks[0]).toMatchObject(output);
   });
+});
+
+describe('Remove a task method', () => {
+  // Arrange
+  let tasks = [
+    { description: 'New Task', index:1, completed: false },
+    { description: 'Second Task', index:2, completed: true },
+    { description: 'Third Task', index: 3, completed: false }];
+
+  const output = [{description:'New Task',index:1,completed: false},
+  {description:'Third Task',index:3,completed: false}];
+
+  //act
+  tasks = removeTask(tasks, 2);
+
+  //Assert
+  test('Delete an item', () => {
+    expect(tasks).toMatchObject(output);
+  });
+
+  // Act and Assert
+  test('should throw an Error if index not Exist', () => {
+    expect(removeTask(tasks, 2)).toThrow(Error);
+    expect(removeTask(tasks, 4)).toThrow(Error);
+    expect(removeTask(tasks, 0)).toThrow(Error);
+  });
+
+  // Act 
+  tasks = removeTask(tasks, 3);
+  tasks = removeTask(tasks, 1);
+  
+  //Assert
+  test('Throw Error when deleting from an Emppty Array', () => {
+    expect(removeTask(tasks, 1)).toThrow('You cant delete from an empty array');
+  });
+
 });
