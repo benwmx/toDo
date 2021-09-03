2a6d61eda9e03e3817da4fac7e090d36b4db5cbc
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-09-03 14:45:50 +0430

    Rearranging tests

--- src/test/task.test.js
+++ src/test/task.test.js
@@ -5,101 +5,61 @@ import localStorage from '../__mocks__/localStorage.js';
 
 jest.mock('../task');
 
+// Arrange
+let tasks = [
+  { description: 'New Task', index: 1, completed: false },
+  { description: 'Second Task', index: 2, completed: true },
+];
+let output;
+
 describe('Add a task method', () => {
-  // Arrange
-  const tasks = [];
-  const description = 'the first task';
-  const output = { description, index: 1, completed: false };
-
-  // Act
-  addTask(tasks, description);
-  // Assert
-  test('Add a one task', () => {
-    expect(tasks.length).toBe(1);
+  test('Add one task', () => {
+    // Arrange
+    const description = 'Third Task';
+    output = { description, index: 3, completed: false };
+    // Act
+    addTask(tasks, description);
+    // Assert
+    expect(tasks.length).toBe(3);
   });
-
   test('Add the correct object', () => {
-    expect(tasks[0]).toMatchObject(output);
+    expect(tasks[2]).toMatchObject(output);
+  });
+  test('Check if the Item has been added to local Storage', () => {
+    // Act
+    updateStorage(tasks);
+    // Assert
+    expect(localStorage.getItem('storage')).toMatchObject(tasks);
   });
 });
 
 describe('Delete a task method', () => {
-  // Arrange
-  let tasks = [
-    { description: 'New Task', index: 1, completed: false },
-    { description: 'Second Task', index: 2, completed: true },
-    { description: 'Third Task', index: 3, completed: false }];
-
-  const expectedOutPut = [{ description: 'New Task', index: 1, completed: false },
-    { description: 'Third Task', index: 3, completed: false }];
-
-  // Act
-  tasks = removeTask(tasks, 2);
-  // Assert
   test('Delete a task', () => {
+    // Arrange
+    output = [{ description: 'New Task', index: 1, completed: false },
+      { description: 'Third Task', index: 3, completed: false }];
+    // Act
+    tasks = removeTask(tasks, 2);
+    // Assert
     expect(tasks.length).toBe(2);
   });
   test('Delete the exact task', () => {
-    expect(tasks[0]).toMatchObject(expectedOutPut[0]);
-    expect(tasks[1]).toMatchObject(expectedOutPut[1]);
+    expect(tasks[0]).toMatchObject(output[0]);
+    expect(tasks[1]).toMatchObject(output[1]);
   });
-});
-
-describe('Update the indexes of tasks', () => {
-  // Arrange
-  let tasks = [
-    { description: 'New Task', index: 24, completed: false },
-    { description: 'Second Task', index: 0, completed: true },
-    { description: 'Third Task', index: -2, completed: false }];
-
-  let expectedOutPut = [
-    { description: 'New Task', index: 1, completed: false },
-    { description: 'Second Task', index: 2, completed: true },
-    { description: 'Third Task', index: 3, completed: false }];
-
-  // Act
-  orderTasks(tasks);
-
-  // Assert
   test('Order the tasks with unordered indexes', () => {
-    expect(tasks).toMatchObject(expectedOutPut);
+    // Arrange
+    output = [{ description: 'New Task', index: 1, completed: false },
+      { description: 'Third Task', index: 2, completed: false }];
+    // Act
+    orderTasks(tasks);
+    // Assert
+    expect(tasks).toMatchObject(output);
   });
-  // Arrange
-  tasks = [];
-  expectedOutPut = [];
-
-  // Act
-  orderTasks(tasks);
-
-  // Assert
-  test('input an empty array', () => {
-    expect(tasks).toMatchObject(expectedOutPut);
-  });
-});
-
-describe('update the local Storage', () => {
-  // Arrange
-  let tasks = [
-    { description: 'New Task', index: 1, completed: false },
-    { description: 'Second Task', index: 2, completed: true },
-    { description: 'Third Task', index: 3, completed: false }];
-
-  // Act
-  updateStorage(tasks);
-
-  // Assert
-  test('input an array of tasks', () => {
+  test('Check if the Item has been Deleted from Local Storage', () => {
+    // Act
+    updateStorage(tasks);
+    // Assert
     expect(localStorage.getItem('storage')).toMatchObject(tasks);
-  });
-
-  // Arrange
-  tasks = [];
-
-  // Act
-  updateStorage(tasks);
-
-  // Assert
-  test('input an empty array', () => {
-    expect(localStorage.getItem('storage')).toMatchObject([]);
   });
 });
